cd037437ec36e483cd72255cef87163b4a933113
unknown <ayt.ullah.dev@gmail.com>
2022-05-24 17:11:09 +0600

    Payments system added

--- src/Components/Sections/Products/Checkout.js
+++ src/Components/Sections/Products/Checkout.js
@@ -1,11 +1,98 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { error } from "daisyui/src/colors";
+import axios from "axios";
+const Checkout = ({price}) => {
+   const [carderror, setCarderror] = useState(null)
+  const stripe = useStripe();
+  const elements = useElements();
+  const [clientsecreat,setClientsecreat]=useState('');
+  useEffect(() => {
+    axios.post("http://localhost:5000/create-payment-intent",{price:price})
+    .then(res=>{
+      setClientsecreat(res.data.clientSecret)
+    })
+  }, [price])
 
-const Checkout = () => {
-    return (
-        <div>
-            Check out
-        </div>
-    );
+  const handleSubmit = async (event) => {
+    // Block native form submission.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
+    }
+
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
+    // Use your card Element with other Stripe.js APIs
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      setCarderror(error.message)
+    } else {
+        setCarderror(null)
+        //Confirm card payment
+        const {paymentIntent,error: intentError} = await stripe.confirmCardPayment(clientsecreat,{
+          payment_method:{
+            card:card,
+            billing_details:{
+              email:"ayt.ullah.dev@gmail.com",
+            }
+          },
+        });
+        if(intentError){
+          setCarderror(intentError?.message);
+        }else{
+          setCarderror('');
+          console.log("Sucessfully Payment");
+        }
+    }
+  };
+
+  return (
+    <div className="mx-10 rounded p-5">
+      <form className=" bg-white p-5" onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          type="submit"
+          className="btn bg-green-500 border-none hover:bg-green-600"
+          disabled={!stripe || !clientsecreat}
+        >
+          Pay
+        </button>
+        {carderror && <p className="text-sm text-red-400">{carderror}</p>}
+      </form>
+      
+    </div>
+  );
 };
 
 export default Checkout;
--- src/Components/Sections/Products/Orderfrom.js
+++ src/Components/Sections/Products/Orderfrom.js
@@ -2,69 +2,97 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import Checkout from "./Checkout";
-const Orderfrom = ({product}) => {
-    const [showcheckout, setShowcheckout] = useState(false)
-    const { register, handleSubmit, reset, formState: { errors } } = useForm();
-    const onSubmit = data =>{
-       console.log(data);
-       setShowcheckout(true);
-    };
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+const stripePromise = loadStripe(
+  "pk_test_51L2sXgF65j8JGYI7Esqw7JbRwzdYqdwhSDkR3Nc0pKE4ormrjsxeJSRWgZmflJmc8F8Iazdshcy0QaUEATogVfCd00tunE8JIm"
+);
+const Orderfrom = ({ product }) => {
+  const [showcheckout, setShowcheckout] = useState(false);
+  const [price,setPrice]= useState(0)
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    setPrice(product.price*data.quan);
+    setShowcheckout(true);
+  };
   return (
     <div>
-     {!showcheckout && <div class="card flex-shrink-0 w-full max-w-sm rounded-none bg-base-100">
-        <form onSubmit={handleSubmit(onSubmit)} class="card-body">
-        <h1>ORDER NOW</h1>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Email</span>
-            </label>
-            <input
-              type="text"
-              placeholder="email"
-              {...register("email",{required:true})}
-              class="input input-bordered"
-            />
-            <label className=" label text-red-500">
+      {!showcheckout && (
+        <div class="card flex-shrink-0 w-full max-w-sm rounded-none bg-base-100">
+          <form onSubmit={handleSubmit(onSubmit)} class="card-body">
+            <h1>ORDER NOW</h1>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                {...register("email", { required: true })}
+                class="input input-bordered"
+              />
+              <label className=" label text-red-500">
                 {errors.email && <span>This filed is requres</span>}
-            </label>
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Adress</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Location"
-              {...register("adress",{required:true})}
-              class="input input-bordered"
-            />
-            <label className=" label text-red-500">
+              </label>
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Adress</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Location"
+                {...register("adress", { required: true })}
+                class="input input-bordered"
+              />
+              <label className=" label text-red-500">
                 {errors.adress && <span>This filed is requres</span>}
-            </label>
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Quantity</span>
-            </label>
-            <input
-              type="number"
-              placeholder="Quantity"
-              {...register("quan",{required:true,min:product.quan<= 5?product.quan:5})}
-              defaultValue={product.quan<= 5?product.quan:5}
-              class="input input-bordered"
-            />
-            <label className=" label text-red-500">
-                {errors.quan && <span>Minium Quantity is {product.quan<= 5?product.quan:5}</span>}
-            </label>
-           
-          </div>
-          <div class="form-control mt-6">
-            <button class="btn bg-green-500 border-none hover:bg-green-600">Check OUT</button>
-          </div>
-        </form>
-      </div>
-     }
-     {showcheckout && <Checkout/>}
+              </label>
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Quantity</span>
+              </label>
+              <input
+                type="number"
+                placeholder="Quantity"
+                {...register("quan", {
+                  required: true,
+                  min: product.quan <= 5 ? product.quan : 5,
+                  max: product.quan,
+                })}
+                defaultValue={product.quan <= 5 ? product.quan : 5}
+                class="input input-bordered"
+              />
+              <label className=" label text-red-500">
+                {errors.quan && errors.quan.type === "min" && (
+                  <span>
+                    Minium Quantity is {product.quan <= 5 ? product.quan : 5}
+                  </span>
+                )}
+                {errors.quan && errors.quan.type === "max" && (
+                  <span>maximum Quantity is {product.quan}</span>
+                )}
+              </label>
+            </div>
+            <div class="form-control mt-6">
+              <button class="btn bg-green-500 border-none hover:bg-green-600">
+                Check OUT
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+      {showcheckout && (
+        <Elements stripe={stripePromise}>
+          <Checkout price={price}/>
+        </Elements>
+      )}
     </div>
   );
 };
